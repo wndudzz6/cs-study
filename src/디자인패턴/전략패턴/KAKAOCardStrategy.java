02168752e31a4095212431556897077c38ef185a
wndudzz6 <osmzxcvb@naver.com>
2025-10-05 00:16:20 +0900

    cs공부 시작

---
+++ src/디자인패턴/전략패턴/KAKAOCardStrategy.java
@@ -0,0 +1,8 @@
+package 디자인패턴.전략패턴;
+
+public class KAKAOCardStrategy implements PaymentStrategy{
+    @Override
+    public void pay(int amount) {
+        System.out.println();
+    }
+}
---
+++ src/디자인패턴/전략패턴/PaymentStrategy.java
@@ -0,0 +1,5 @@
+package 디자인패턴.전략패턴;
+
+public interface PaymentStrategy {
+    public void pay(int amount);
+}
